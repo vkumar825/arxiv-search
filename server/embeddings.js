3d98e371827aa0a5ab9cfbe6f3393b840b518d1a
vkumar825 <vkumar01825@gmail.com>
2026-04-19 17:48:09 -0400

    implemented getEmbeddedObjs

--- server/embeddings.js
+++ server/embeddings.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { pipeline } from "@huggingface/transformers";
+import crypto from "node:crypto";
+
 process.loadEnvFile();
 
 let pipelinePromise = null;
@@ -10,12 +12,47 @@ export const getPipelineInstance = async () => {
     return pipelinePromise;
   }
 
-  pipelinePromise = pipeline(process.env.MODEL_TASK, process.env.MODEL_NAME)
-    .catch(error => {
-            pipelinePromise = null; 
-            console.error("Failed to initialize pipeline:", error);
-            throw error;
-        });
+  pipelinePromise = pipeline(
+    process.env.MODEL_TASK,
+    process.env.MODEL_NAME,
+  ).catch((error) => {
+    pipelinePromise = null;
+    console.error("Failed to initialize pipeline:", error);
+    throw error;
+  });
 
   return pipelinePromise;
+};
+
+export const getEmbeddedObjs = async (pipeline, objects) => {
+  const texts = objects.map((obj) => obj.short_description);
+  const embeddedObjs = [];
+
+  const output = await pipeline(texts, {
+    pooling: "mean",
+    normalize: true,
+  });
+
+  const embeddings = output.tolist();
+
+  if (embeddings.length !== texts.length) {
+    throw new Error(
+      `Embedding mismatch: Got ${embeddings.length} embeddings for ${objects.length} texts.`,
+    );
+  }
+
+  const createId = (headline) => {
+    return crypto.createHash('sha256').update(headline).digest('hex');
+  }
+
+  for (let i = 0; i < objects.length; i++) {
+    embeddedObjs.push({
+      id: createId(objects[i].headline),
+      headline: objects[i].headline,
+      vector: embeddings[i],
+      category: objects[i].category,
+    });
+  }
+
+  return embeddedObjs;
 };

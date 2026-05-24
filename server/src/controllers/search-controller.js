f31dbfb7be668501662bca5514418ef592ea018d
vkumar825 <vkumar01825@gmail.com>
2026-05-24 19:45:57 -0400

    expanded handleSearchRequest

--- server/src/controllers/search-controller.js
+++ server/src/controllers/search-controller.js
@@ -2,12 +2,23 @@ import { getSearchResults } from "../service/search-service.js";
 
 export const handleSearchRequest = async (req, res) => {
   try {
-    const term = req.body.term;
+    const term = req.query.term;
+    const limit = parseInt(req.query.limit) || 10;
+    const filter = req.query.filter || "";
 
-    const searchResults = await getSearchResults(term);
-    
-    res.render("search.ejs", {results: searchResults.results});
+    const searchResults = await getSearchResults(term, limit, filter);
 
+    if (req.baseUrl === "/api/v1") {
+      if (!term || term === "") {
+        return res.status(400).json({ error: "Search term is required" });
+      }
+      // exclude vector field from the results (because it makes it messy to read)
+      const sanitizedResults = searchResults.results.map((item) => {
+        const { vector, ...rest } = item;
+        return rest;
+      });
+      return res.json({ results: sanitizedResults });
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }

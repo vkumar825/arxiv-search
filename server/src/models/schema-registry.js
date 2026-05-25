ca361453498b49114997947c3b4bc17ff1a1436e
Vivek Kumar <vkumar01825@gmail.com>
2026-05-25 18:05:46 -0400

    switch to dictionary lookup

--- server/src/models/schema-registry.js
+++ server/src/models/schema-registry.js
@@ -1,8 +1,15 @@
 import { SandboxSchema } from "./milvus-schema.js";
 
+const schemaRegistry = {
+  sandbox: SandboxSchema,
+};
+
 export const retreiveSchemaInfo = (schemaType) => {
-  switch (schemaType) {
-    case "sandbox":
-      return SandboxSchema;
+  const schema = schemaRegistry[schemaType];
+
+  if (!schema) {
+    throw new Error(`Schema ${schemaType} not found in the registry.`);
   }
+
+  return schema;
 };

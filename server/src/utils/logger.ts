c47ab9d736b0909e0a8ad5f561624a22727d6d44
Vivek Kumar <vkumar01825@gmail.com>
2026-09-16 01:31:57 -0400

    make two separate loggers for server, and ingestion

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -2,24 +2,31 @@ import pino from "pino";
 
 process.loadEnvFile();
 
-const dest = pino.destination({
-  dest: process.env.PINO_LOG_FILE_PATH as string,
+const serverDest = pino.destination({
+  dest: process.env.PINO_SERVER_LOG_PATH as string,
+  sync: false,
+  mkdir: true,
+});
+
+const ingestionDest = pino.destination({
+  dest: process.env.PINO_INGESTION_LOG_PATH as string,
   sync: true,
   mkdir: true,
 });
 
-const logger = pino(
-  {
-    level: process.env.PINO_LOG_LEVEL,
-    base: undefined,
-    formatters: {
-      level: (label: string) => {
-        return { level: label.toUpperCase() };
-      },
+const baseOptions = {
+  level: process.env.PINO_LOG_LEVEL,
+  base: null,
+  formatters: {
+    level: (label: string) => {
+      return { level: label.toUpperCase() };
     },
-    timestamp: pino.stdTimeFunctions.isoTime,
   },
-  dest,
-);
+  timestamp: pino.stdTimeFunctions.isoTime,
+};
 
-export default logger;
+const server = pino(baseOptions, serverDest);
+
+const ingestion = pino(baseOptions, ingestionDest);
+
+export { server, ingestion };
